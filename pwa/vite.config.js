1c6987a4b02bf39c5fce4397ea5be07e3a497a0e
John-Weak <tusharbali11@gmail.com>
2023-03-25 20:12:16 +0530

    [feature] basic pwa support

--- pwa/vite.config.js
+++ pwa/vite.config.js
@@ -1,6 +1,48 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			},
+			injectManifest: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			workbox: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			manifest: {
+				short_name: 'PCS',
+				name: 'Project Car Scraping',
+				description:
+					'Consolidating for sale modded/enthusiast vehicles in one place from different threads/websites',
+				theme_color: '#000000',
+				background_color: '#000000',
+				icons: [
+					{
+						src: '/android-chrome-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: '/android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						src: '/android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable'
+					}
+				]
+			}
+		})
+	]
 });
